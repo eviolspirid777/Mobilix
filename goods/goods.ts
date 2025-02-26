7c0f7158a9b2a54cefb86d5da4cba13902132569
Kiruha <93467283+eviolspirid777@users.noreply.github.com>
2025-02-26 13:49:28 +0300

    Исправление товаров, исправление стилей

--- goods/goods.ts
+++ goods/goods.ts
@@ -5,72 +5,156 @@ export const goods: ProductCard[] = [
         image: "/iphones/Iphone16ProMaxWhite.png",
         memory: "1 Тб",
         name: "Iphone 16 Pro Max",
-        price: 100900
+        price: 100900,
+        accumulator: "4555",
+        camera: {
+            back: "50",
+            front: "20"
+        },
+        color: "Красный",
+        simCard: "eSim + sim"
     },
     {
         image: "/iphones/Iphone16ProMaxWhite.png",
         memory: "512 Гб",
         name: "Iphone 16 Pro Max",
-        price: 100900
+        price: 100900,
+        accumulator: "4555",
+        camera: {
+            back: "50",
+            front: "20"
+        },
+        color: "Красный",
+        simCard: "eSim + sim"
     },
     {
         image: "/iphones/Iphone16ProMaxWhite.png",
         memory: "256 Гб",
         name: "Iphone 16 Pro Max",
-        price: 100900
+        price: 100900,
+        accumulator: "4555",
+        camera: {
+            back: "50",
+            front: "20"
+        },
+        color: "Красный",
+        simCard: "eSim + sim"
     },
     {
         image: "/iphones/Iphone16ProMaxWhite.png",
         memory: "128 Гб",
         name: "Iphone 16 Pro Max",
-        price: 100900
+        price: 100900,
+        accumulator: "4555",
+        camera: {
+            back: "50",
+            front: "20"
+        },
+        color: "Красный",
+        simCard: "eSim + sim"
     },
     {
         image: "/iphones/Iphone16Green.png",
         memory: "128 Гб",
         name: "Iphone 16",
-        price: 100900
+        price: 100900,
+        accumulator: "4555",
+        camera: {
+            back: "50",
+            front: "20"
+        },
+        color: "Красный",
+        simCard: "eSim + sim"
     },
     {
         image: "/iphones/Iphone16Green.png",
         memory: "256 Гб",
         name: "Iphone 16",
-        price: 100900
+        price: 100900,
+        accumulator: "4555",
+        camera: {
+            back: "50",
+            front: "20"
+        },
+        color: "Красный",
+        simCard: "eSim + sim"
     },
     {
         image: "/iphones/Iphone16Blue.png",
         memory: "256 Гб",
         name: "Iphone 16",
-        price: 100900
+        price: 100900,
+        accumulator: "4555",
+        camera: {
+            back: "50",
+            front: "20"
+        },
+        color: "Красный",
+        simCard: "eSim + sim"
     },
     {
         image: "/iphones/Iphone16Blue.png",
         memory: "1 тб",
         name: "Iphone 16",
-        price: 100900
+        price: 100900,
+        accumulator: "4555",
+        camera: {
+            back: "50",
+            front: "20"
+        },
+        color: "Красный",
+        simCard: "eSim + sim"
     },
     {
         image: "/iphones/Iphone15ProMax.png",
         memory: "1 Тб",
         name: "Iphone 15 Pro Max",
-        price: 100900
+        price: 100900,
+        accumulator: "4555",
+        camera: {
+            back: "50",
+            front: "20"
+        },
+        color: "Красный",
+        simCard: "eSim + sim"
     },
     {
         image: "/iphones/Iphone15ProMax.png",
         memory: "512 Гб",
         name: "Iphone 15 Pro Max",
-        price: 100900
+        price: 100900,
+        accumulator: "4555",
+        camera: {
+            back: "50",
+            front: "20"
+        },
+        color: "Красный",
+        simCard: "eSim + sim"
     },
     {
         image: "/iphones/Iphone15ProMax.png",
         memory: "256 Гб",
         name: "Iphone 15 Pro Max",
-        price: 100900
+        price: 100900,
+        accumulator: "4555",
+        camera: {
+            back: "50",
+            front: "20"
+        },
+        color: "Красный",
+        simCard: "eSim + sim"
     },
     {
         image: "/iphones/Iphone15Pro.png",
         memory: "256 Гб",
         name: "Iphone 15 Pro",
-        price: 100900
+        price: 100900,
+        accumulator: "4555",
+        camera: {
+            back: "50",
+            front: "20"
+        },
+        color: "Красный",
+        simCard: "eSim + sim"
     },
 ]
