ecdc4b325f0914261cd3140a444c0cfe17737a31
eviolspirid <eviolspirid@gmail.com>
2025-03-01 12:34:14 +0300

    сборка готова под выгрузку

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,18 @@ import type { NextConfig } from "next";
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const withTM = require('next-transpile-modules')(['rc-util']);
 
+type ConfigType = {
+  experiments: object,
+  resolve: {
+    alias: Record<string,string>
+  }
+}
+
+
 const nextConfig: NextConfig = withTM({
   reactStrictMode: true,
   transpilePackages: ['rc-picker', 'rc-pagination', 'antd'],
-  webpack: (config, { isServer }: {isServer: boolean}) => {
+  webpack: (config: ConfigType, { isServer }: {isServer: boolean}) => {
     if (!isServer) {
       config.experiments = {
         ...(config.experiments || {}),
